ad42a87823ef9d64a17fd3cca614cdb883880b6d
rckrieger <rebecca.c.krieger@gmail.com>
2021-06-23 15:49:12 -0700

    Initial Commit

---
+++ gestures for swift ui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gestures for swift ui
+//
+//  Created by Rebecca Krieger on 6/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gestures for swift ui/gestures_for_swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  gestures_for_swift_uiApp.swift
+//  gestures for swift ui
+//
+//  Created by Rebecca Krieger on 6/23/21.
+//
+
+import SwiftUI
+
+@main
+struct gestures_for_swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
